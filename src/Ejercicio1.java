02ff5112e9f64cd8076d2ad70e73e7833fb55f33
Sebastian Arenillas - Gabriel Cuenca <sebasarenillas10@gmail.com>
2026-06-08 10:59:22 -0500

    u2

---
+++ src/Ejercicio1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Ejercicio1 {
+    ///
+    /// Metodo que devuelve una cadena de
+    /// texto invertida
+    /// texto -> COMPUTACION
+    /// return -> NOICATUPMOC
+    /// USANDO SOLO PILAS
+
+
+    public String invertString(String texto){
+
+        Deque<Character> pila = new ArrayDeque<>();
+        for(char letra: texto.toCharArray()){
+            pila.push(letra);
+        }
+        String invertido="";
+
+        while(!pila.isEmpty()){
+            invertido += pila.pop();
+        }
+        return invertido;
+    }
+}
